59634846c59e56afb84b4186411034cbce8125dd
pieda <pieda@LAPTOP-MVBH5M4M>
2023-05-18 21:49:21 +0200

    probando test de crear un curso vacio sin id y creaer un curso vacio sin guardarlo

--- extraf/src/test/java/iso/extraf/controllers/GestorPropuestaCursoTest.java
+++ extraf/src/test/java/iso/extraf/controllers/GestorPropuestaCursoTest.java
@@ -1,12 +1,20 @@
 package iso.extraf.controllers;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+
 import java.util.Date;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Spy;
+
 import iso.extraf.negocio.controllers.GestorPropuestasCursos;
 import iso.extraf.negocio.entities.CursoPropio;
+import iso.extraf.persistencia.CursoPropioDAO;
 import static org.junit.Assert.assertTrue;
+
 
 public class GestorPropuestaCursoTest{
 		GestorPropuestasCursos gpc;
@@ -17,12 +25,25 @@ public class GestorPropuestaCursoTest{
 		}
 		
 		@Test
-		public void realizarPropuestaCurso_debe_crear_un_curso_por_defecto_sin_id() {
+		public void realizarPropuestaCurso_debe_crear_un_curso_vacio_sin_id() {
 			//When
 			CursoPropio cp = gpc.realizarPropuestaCurso();
 			
 			//Then
 			org.junit.Assert.assertNotNull(cp);
-			org.junit.Assert.assertNull(cp);
+			org.junit.Assert.assertNull(cp.getId());
+		}
+		
+		@Test
+		public void realizarPropuestaCurso_debe_crear_un_curso_vacio_sin_persistirlo() {
+			//Given
+			CursoPropioDAO dao = new CursoPropioDAO(); 
+			CursoPropioDAO daoSpy = spy(dao);
+			
+			//When
+			CursoPropio cp = gpc.realizarPropuestaCurso(daoSpy);
+			
+			//Then
+			verifyNoInteractions(daoSpy);
 		}
 }
