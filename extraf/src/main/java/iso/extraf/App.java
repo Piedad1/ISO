e2ad0fe57e66c651f3c1786b851f609b3e5a42dc
Piedad1 <91836109+Piedad1@users.noreply.github.com>
2023-05-24 23:07:16 +0200

    Mejorando App.java

--- extraf/src/main/java/iso/extraf/App.java
+++ extraf/src/main/java/iso/extraf/App.java
@@ -1,11 +1,29 @@
 package iso.extraf;
 
+import java.util.List;
+
+import iso.extraf.persistencia.GestorBD;
+
 /**
  * Hello world!
  *
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		GestorBD gbd = GestorBD.conectarBD();
+		
+		List<Object[]> resultado = gbd.select("SELECT * FROM cursospropios");
+		
+		System.out.println(resultado.size());
+		
+		for (Object[] fila : resultado) {
+			
+			for (Object columna : fila) {
+				System.out.print(columna + "\t");
+			}
+			
+			System.out.println();
+		}
 	}
 }
+
